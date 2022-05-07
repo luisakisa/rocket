938bcb81cdda7613ea42567f95c5d32c6fe3520e
luisakisa <72152407+luisakisa@users.noreply.github.com>
2022-05-07 13:23:53 +0700

    killmeplease

--- Rocket/src/main/module-info.java
+++
@@ -1,9 +0,0 @@
-module application {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    //if necessary, please activate:
-    //exports application;
-    //requires transitive javafx.graphics;
-
-}
